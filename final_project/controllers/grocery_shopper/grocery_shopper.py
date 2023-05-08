ec0b821b5c4cba0542c769bf263b6e8bd94a404a
PeterMcD1 <pemc6694@colorado.edu>
2023-05-07 22:21:46 -0600

    moved groceryshopper.py

--- final_project/controllers/grocery_shopper/grocery_shopper.py
+++ final_project/controllers/grocery_shopper/grocery_shopper.py
@@ -2,14 +2,19 @@
 
 # Nov 2, 2022
 
-from controller import Robot
+from controller import Robot, Motor, Camera, RangeFinder, Lidar, Keyboard
 import math
 import numpy as np
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+import ikpy.utils.plot as plot_utils
+import matplotlib.pyplot
+from mpl_toolkits.mplot3d import Axes3D
 import cv2
-
+import ipywidgets as widgets
 
 #Initialization
-print("=== Initializing Grocery Shopper...")
+print("=== Initializing Grocery Shopper...\n")
 #Consts
 MAX_SPEED = 7.0  # [rad/s]
 MAX_SPEED_MS = 0.633 # [m/s]
@@ -33,7 +38,6 @@ part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
               "arm_6_joint",  "arm_7_joint",  "wheel_left_joint", "wheel_right_joint",
               "gripper_left_finger_joint","gripper_right_finger_joint")
 
-# 
 
 # All motors except the wheels are controlled by position control. The wheels
 # are controlled by a velocity controller. We therefore set their position to infinite.
@@ -44,6 +48,8 @@ for i, part_name in enumerate(part_names):
     robot_parts[part_name]=robot.getDevice(part_name)
     robot_parts[part_name].setPosition(float(target_pos[i]))
     robot_parts[part_name].setVelocity(robot_parts[part_name].getMaxVelocity() / 2.0)
+    position_sensor = robot_parts[part_name].getPositionSensor()
+    position_sensor.enable(timestep)
 
 # Enable gripper encoders (position sensors)
 left_gripper_enc=robot.getDevice("gripper_left_finger_joint_sensor")
@@ -67,10 +73,6 @@ lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
 lidar.enable(timestep)
 lidar.enablePointCloud()
 
-# We are using a keyboard to remote control the robot
-keyboard = robot.getKeyboard()
-keyboard.enable(timestep)
-
 # Enable display
 display = robot.getDevice("display")
 
@@ -88,73 +90,187 @@ lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readin
 
 map = None
 
-mode = 'manual'
+# We are using a keyboard to remote control the robot
+keyboard = robot.getKeyboard()
+keyboard.enable(timestep)
+
+print("=== Initialization Complete!\n")
+
+
+###############
+# IKPY setup
+###############
+print("=== Setting up IKPY...\n")
+
+# get the URDF file **UNCOMMENT AND RUN ONLY ONCE TO INITIALIZE**
+# with open("tiago_urdf.urdf", "w") as file:  
+    # file.write(robot.getUrdf())
+
+# create chain for the robot
+base_elements=["base_link", "base_link_Torso_joint", "Torso", "torso_lift_joint", "torso_lift_link", 
+               "torso_lift_link_TIAGo front arm_11367_joint", "TIAGo front arm_11367"]
+
+# include finger link (end-effector) to chain
+my_chain = Chain.from_urdf_file("robot_urdf.urdf", last_link_vector=[0.004, 0, -0.1741], 
+            base_elements=["base_link", "base_link_Torso_joint", "Torso", "torso_lift_joint", "torso_lift_link", 
+                           "torso_lift_link_TIAGo front arm_11367_joint", "TIAGo front arm_11367"])
+                                    
+for link_id in range(len(my_chain.links)):
+    # link obj
+    link = my_chain.links[link_id]
+    # ignore "torso_lift_joint"
+    if link.name not in part_names or  link.name == "torso_lift_joint":
+        my_chain.active_links_mask[link_id] = False
+        
+# Initialize the arm motors and encoders.
+motors = []
+for link in my_chain.links:
+    if link.name in part_names and link.name != "torso_lift_joint":
+        motor = robot.getDevice(link.name)
+
+        # Make sure to account for any motors that
+        # require a different maximum velocity!
+        if link.name == "torso_lift_joint":
+            motor.setVelocity(0.07)
+        else:
+            motor.setVelocity(1)
+            
+        position_sensor = motor.getPositionSensor()
+        position_sensor.enable(timestep)
+        motors.append(motor)
+                          
+print(my_chain.links)
+
+# disable torso lift joint
+# my_chain.active_links_mask[1] = False
+
+# get intial position **GIVES NAN VALUES ON FIRST RUN -- RUN TWICE**
+initial_position = [0,0,0,0] + [m.getPositionSensor().getValue() for m in motors] + [0,0,0,0]
+print(initial_position)
+
+print("=== IKPY Setup Complete!\n")
 
 # ------------------------------------------------------------------
 # Helper Functions
 
-    
-gripper_status="closed"
+mode = "manual"
+
+# open grippers
+robot_parts["gripper_left_finger_joint"].setPosition(0.045)
+robot_parts["gripper_right_finger_joint"].setPosition(0.045)
+if left_gripper_enc.getValue()>=0.044:
+    gripper_status="open"
+
+# set torso_lift_joint to 0
+robot.getDevice("torso_lift_joint").setPosition(0.25)
+error = 0
+c = 0
 
 # Main Loop
 while robot.step(timestep) != -1:
-    if mode == 'manual':
+    
+    ###################
+    #
+    # Controller
+    #
+    ###################
+    if mode == "manual":
         key = keyboard.getKey()
         while(keyboard.getKey() != -1): pass
-        if key == keyboard.LEFT :
+        if key == ord('A'):
+            vL = -MAX_SPEED
+            vR = MAX_SPEED
+        elif key == ord('D'):
+            vL = MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == ord('W'):
+            vL = MAX_SPEED
+            vR = MAX_SPEED
+        elif key == ord('S'):
+            vL = -MAX_SPEED
+            vR = -MAX_SPEED
+        elif key == keyboard.LEFT:
             vL = -MAX_SPEED
             vR = MAX_SPEED
         elif key == keyboard.RIGHT:
-            vL = MAX_SPEED
-            vR = -MAX_SPEED
+            if(gripper_status=="open"):
+                # Close gripper, note that this takes multiple time steps...
+                robot_parts["gripper_left_finger_joint"].setPosition(0)
+                robot_parts["gripper_right_finger_joint"].setPosition(0)
+                gripper_status="closed"
+            else:
+                # Open gripper
+                robot_parts["gripper_left_finger_joint"].setPosition(0.045)
+                robot_parts["gripper_right_finger_joint"].setPosition(0.045)
+                gripper_status="open"
         elif key == keyboard.UP:
-            vL = MAX_SPEED
-            vR = MAX_SPEED
+            # get intial position, disabled links have position '0'
+            initial_position = [0,0,0,0] + [m.getPositionSensor().getValue() for m in motors] + [0,0,0,0]
+            # get yellow obj position relative to the camera
+            target = yellowobjsGPS[c]
+            print(target)
+            
+            # adjust target from camera coordinate to be realtive to the end effector
+            adjusted_target = [-target[2] + 0.47, -target[0] + 0.87, target[1] + 0.90]
+            # ik operation
+            ikResults = my_chain.inverse_kinematics(adjusted_target, target_orientation = [0,0,1], orientation_mode="Y")
+            # apply ik to robot's end effector
+            for res in range(len(ikResults)):
+                # ignore non-controllable links
+                if my_chain.links[res].name in part_names:
+                    if my_chain.links[res].name == "torso_lift_joint":
+                        robot.getDevice(my_chain.links[res].name).setPosition(0.25)
+                        print("Setting {} to {}".format(my_chain.links[res].name, 0.25))
+                    elif my_chain.links[res].name == "gripper_right_finger_joint":
+                        robot.getDevice(my_chain.links[res].name).setPosition(0.045)
+                        print("Setting {} to {}".format(my_chain.links[res].name, 0.045))
+                    else:
+                        robot.getDevice(my_chain.links[res].name).setPosition(ikResults[res])
+                        print("Setting {} to {}".format(my_chain.links[res].name, ikResults[res]))
+            c = c + 1
+            
         elif key == keyboard.DOWN:
-            vL = -MAX_SPEED
-            vR = -MAX_SPEED
-        elif key == ord(' '):
-            vL = 0
-            vR = 0
-        elif key == ord('S'):
+            # get intial position, disabled links have position '0'
+            initial_position = [0,0,0,0] + [m.getPositionSensor().getValue() for m in motors] + [0,0,0,0]
+            # send arm to directly above basket
+            target = [0.3, 0.0, 0.8]
+            print(target)
+
+            # ik operation
+            ikResults = my_chain.inverse_kinematics(target, target_orientation = [0,0,1], orientation_mode="Y")
+            # apply ik to robot's end effector
+            for res in range(len(ikResults)):
+                # ignore non-controllable links
+                if my_chain.links[res].name in part_names:
+                    if my_chain.links[res].name == "torso_lift_joint":
+                        robot.getDevice(my_chain.links[res].name).setPosition(0.25)
+                        print("Setting {} to {}".format(my_chain.links[res].name, 0.25))
+                    elif my_chain.links[res].name == "gripper_right_finger_joint":
+                        robot.getDevice(my_chain.links[res].name).setPosition(0.045)
+                        print("Setting {} to {}".format(my_chain.links[res].name, 0.045))
+                    else:
+                        robot.getDevice(my_chain.links[res].name).setPosition(ikResults[res])
+                        print("Setting {} to {}".format(my_chain.links[res].name, ikResults[res]))
+        # elif key == ord('O'):
             # Part 1.4: Filter map and save to filesystem
-            filtered_map = np.multiply(map > 0.5, 1)
-            np.save("map.npy", filtered_map)
-            print("Map file saved")
-        elif key == ord('L'):
+            # filtered_map = np.multiply(map > 0.5, 1)
+            # np.save("map.npy", filtered_map)
+            # print("Map file saved")
+        # elif key == ord('L'):
             # You will not use this portion in Part 1 but here's an example for loading saved a numpy array
-            map = np.load("map.npy")
-            print("Map loaded")
+            # map = np.load("map.npy")
+            # print("Map loaded")
         else: # slow down
             vL *= 0.75
             vR *= 0.75
-   
+            
     num_objects = camera.getRecognitionNumberOfObjects()
-    yellowobjsGPS = []
-    yellowobjsCamera = []
     obj = camera.getRecognitionObjects()
+    yellowobjsGPS = []
     for i in range(num_objects):
         if (obj[i].getColors() == [1.0, 1.0, 0]):
             yellowobjsGPS.append(obj[i].getPosition())
-            yellowobjsCamera.append(obj[i].getPositionOnImage())
-
-    
-   
-
-
-    
+            #print(yellowobjsGPS)  
+                  
     robot_parts["wheel_left_joint"].setVelocity(vL)
     robot_parts["wheel_right_joint"].setVelocity(vR)
-    
-    if(gripper_status=="open"):
-        # Close gripper, note that this takes multiple time steps...
-        robot_parts["gripper_left_finger_joint"].setPosition(0)
-        robot_parts["gripper_right_finger_joint"].setPosition(0)
-        if right_gripper_enc.getValue()<=0.005:
-            gripper_status="closed"
-    else:
-        # Open gripper
-        robot_parts["gripper_left_finger_joint"].setPosition(0.045)
-        robot_parts["gripper_right_finger_joint"].setPosition(0.045)
-        if left_gripper_enc.getValue()>=0.044:
-            gripper_status="open"
